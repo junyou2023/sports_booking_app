8a0beedb7fca78d6a9972806499ef376ae39d20b
JUNYOU LUO <2961309L@student.gla.ac.uk>
2025-07-20 18:18:34 +0100

    Update apps.py

--- backend/sports/apps.py
+++ backend/sports/apps.py
@@ -3,6 +3,5 @@ from django.apps import AppConfig
 
 class SportsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    # use the full Python path so Django can load the app regardless of
-    # the current working directory
-    name = 'backend.sports'
+
+    name = 'sports'
