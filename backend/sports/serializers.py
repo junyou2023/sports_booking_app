2e44504bcaba28774c6180988b78d08b82426c25
JUNYOU LUO <2961309L@student.gla.ac.uk>
2025-08-12 00:06:42 +0100

    Fix facility creation with categories

--- backend/sports/serializers.py
+++ backend/sports/serializers.py
@@ -299,12 +299,15 @@ class FacilityCreateSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         lat = validated_data.pop("lat")
         lng = validated_data.pop("lng")
+        categories = validated_data.pop("categories", [])
         point = Point(lng, lat, srid=4326)
         request = self.context.get("request")
         owner = request.user if request else None
         facility = Facility.objects.create(
             location=point, owner=owner, **validated_data
         )
+        if categories:
+            facility.categories.set(categories)
         return facility
 
 
