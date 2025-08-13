383699118d976e043ce5cd72b5974bcda40f5d5a
Junyou Luo <2961309L@student.gla.ac.uk>
2025-08-13 22:26:15 +0100

    feat: make bookings clickable with detail cards

---
+++ lib/widgets/booking_card.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+import '../models/booking.dart';
+
+class BookingCard extends StatelessWidget {
+  final Booking booking;
+  final VoidCallback onTap;
+
+  const BookingCard({super.key, required this.booking, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    final slot = booking.slot;
+    final image = slot.sport.banner;
+    final begins = slot.beginsAt.toLocal();
+    return InkWell(
+      onTap: onTap,
+      child: Card(
+        clipBehavior: Clip.hardEdge,
+        child: Row(
+          children: [
+            image.startsWith('http')
+                ? Image.network(
+              image,
+              width: 120,
+              height: 100,
+              fit: BoxFit.cover,
+              errorBuilder: (_, __, ___) => Image.asset(
+                'assets/images/default.jpg',
+                width: 120,
+                height: 100,
+                fit: BoxFit.cover,
+              ),
+            )
+                : Image.asset(
+              image,
+              width: 120,
+              height: 100,
+              fit: BoxFit.cover,
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(12),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      slot.title,
+                      style: Theme.of(context).textTheme.titleMedium,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      slot.location,
+                      style: Theme.of(context).textTheme.bodySmall,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      '${begins.toString().split(' ')[0]} • ${begins.hour.toString().padLeft(2, '0')}:${begins.minute.toString().padLeft(2, '0')}',
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      'Pax: ${booking.pax}',
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
